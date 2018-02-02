d35b0dd66449a309734f2152fa0b4f7ab22f9075
Emiel van de Laar <gemiel@gmail.com>
2018-02-02 14:26:45 +0100

    Fix naive datetime warnings in tests
    The test settings have USE_TZ=True which results in a number of warnings
    due to naive datetimes in the test case. The naive datetimes have been
    replaced with datetimes built from timezone.now() which respects USE_TZ.
    
    In addition the tests have been refactored a little.

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+from datetime import timedelta
+
 from django.test import TransactionTestCase
+from django.utils import timezone
 
 from django_future.jobs import (schedule_job, run_jobs, _expire_jobs,
                                 _run_scheduled_jobs)
@@ -8,50 +10,55 @@ from django_future.models import ScheduledJob
 
 class JobsTestCase(TransactionTestCase):
 
+    def setUp(self):
+        self.now = timezone.now()
+
     def test_expire_jobs(self):
-        job = schedule_job(datetime(1970, 1, 1), 'math.pow', args=(2, 3))
+        job = schedule_job(
+                self.now - timedelta(hours=1), 'math.pow', args=(2, 3))
         self.assertEqual(job.status, ScheduledJob.STATUS_SCHEDULED)
 
-        _expire_jobs(datetime(1980, 1, 1))
+        _expire_jobs(self.now + timedelta(days=60))
 
-        self.assertEqual(
-            ScheduledJob.objects.get(pk=job.pk).status,
-            ScheduledJob.STATUS_EXPIRED)
+        job.refresh_from_db()
+        self.assertEqual(job.status, ScheduledJob.STATUS_EXPIRED)
 
     def test_run_scheduled_jobs(self):
-        job1 = schedule_job(datetime(1970, 1, 1), 'math.pow', args=(2, 2))
-        job2 = schedule_job(datetime(1970, 1, 2), 'math.pow', args=(2, 3))
+        job1 = schedule_job(
+                self.now - timedelta(hours=2), 'math.pow', args=(2, 2))
+        job2 = schedule_job(
+                self.now - timedelta(hours=1), 'math.pow', args=(2, 3))
 
-        _run_scheduled_jobs(datetime(1970, 1, 3), False, False)
+        _run_scheduled_jobs(self.now, False, False)
 
-        result_job1 = ScheduledJob.objects.get(pk=job1.pk)
-        self.assertEqual(result_job1.status, ScheduledJob.STATUS_COMPLETE)
-        self.assertEqual(result_job1.return_value, u'4.0')
+        job1.refresh_from_db()
+        self.assertEqual(job1.status, ScheduledJob.STATUS_COMPLETE)
+        self.assertEqual(job1.return_value, u'4.0')
 
-        result_job2 = ScheduledJob.objects.get(pk=job2.pk)
-        self.assertEqual(result_job2.status, ScheduledJob.STATUS_COMPLETE)
-        self.assertEqual(result_job2.return_value, u'8.0')
+        job2.refresh_from_db()
+        self.assertEqual(job2.status, ScheduledJob.STATUS_COMPLETE)
+        self.assertEqual(job2.return_value, u'8.0')
 
-        self.assertLess(
-                result_job1.execution_start,
-                result_job2.execution_start)
+        self.assertLess(job1.execution_start, job2.execution_start)
 
     def test_run_jobs_complete(self):
-        job = schedule_job(datetime(1970, 1, 1), 'math.pow', args=(2, 3))
+        job = schedule_job(
+                self.now - timedelta(hours=1), 'math.pow', args=(2, 3))
         self.assertEqual(job.status, ScheduledJob.STATUS_SCHEDULED)
 
-        run_jobs(now=datetime(1970, 1, 2))
+        run_jobs(now=self.now)
 
-        result_job = ScheduledJob.objects.get(pk=job.pk)
-        self.assertEqual(result_job.status, ScheduledJob.STATUS_COMPLETE)
-        self.assertEqual(result_job.return_value, u'8.0')
+        job.refresh_from_db()
+        self.assertEqual(job.status, ScheduledJob.STATUS_COMPLETE)
+        self.assertEqual(job.return_value, u'8.0')
 
     def test_run_jobs_failed(self):
-        job = schedule_job(datetime(1970, 1, 1), 'math.pow')
+        job = schedule_job(
+                self.now - timedelta(hours=1), 'math.pow')
         self.assertEqual(job.status, ScheduledJob.STATUS_SCHEDULED)
 
         with self.assertRaises(TypeError):
-            run_jobs(now=datetime(1970, 1, 2))
+            run_jobs(now=self.now)
 
-        result_job = ScheduledJob.objects.get(pk=job.pk)
-        self.assertEqual(result_job.status, ScheduledJob.STATUS_FAILED)
+        job.refresh_from_db()
+        self.assertEqual(job.status, ScheduledJob.STATUS_FAILED)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+from datetime import timedelta
 
+from django.utils import timezone
 from django.test import TestCase
 
 from django_future.models import ScheduledJob
@@ -7,8 +8,12 @@ from django_future.models import ScheduledJob
 
 class ModelTestCase(TestCase):
 
+    def setUp(self):
+        self.now = timezone.now()
+
     def test_default_status(self):
+        start = self.now
+        end = self.now + timedelta(days=7)
         job = ScheduledJob.objects.create(
-                time_slot_start=datetime(1970, 1, 1),
-                time_slot_end=datetime(1970, 1, 2))
+                time_slot_start=start, time_slot_end=end)
         self.assertEqual(job.status, ScheduledJob.STATUS_SCHEDULED)
