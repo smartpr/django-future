14fa21425e2d1a83c164da2539b2a8b86321f4f9
Emiel van de Laar <gemiel@gmail.com>
2018-02-02 14:47:52 +0100

    Adds ScheduledJob repr test

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,10 +10,21 @@ class ModelTestCase(TestCase):
 
     def setUp(self):
         self.now = timezone.now()
+        self.start = self.now
+        self.end = self.now + timedelta(days=7)
 
     def test_default_status(self):
-        start = self.now
-        end = self.now + timedelta(days=7)
         job = ScheduledJob.objects.create(
-                time_slot_start=start, time_slot_end=end)
+                time_slot_start=self.start, time_slot_end=self.end)
         self.assertEqual(job.status, ScheduledJob.STATUS_SCHEDULED)
+
+    def test_repr(self):
+        job = ScheduledJob.objects.create(
+                time_slot_start=self.start,
+                time_slot_end=self.end,
+                callable_name="func")
+
+        self.assertEqual(
+            "<ScheduledJob (scheduled) callable='func'>",
+            repr(job)
+        )
