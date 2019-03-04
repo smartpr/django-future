b27007d3113fcf73973129515a2800ea07bbe37d
Ties Jan Hefting <tiesjan@smart.pr>
2019-03-04 16:56:28 +0100

    Fix failing model unit tests
    The model's unit tests failed due to NOT NULL constraints. This is fixed
    by giving all required arguments upon ScheduledJob instance creation.

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,22 +9,20 @@ from django_future.models import ScheduledJob
 class ModelTestCase(TestCase):
 
     def setUp(self):
-        self.now = timezone.now()
-        self.start = self.now
-        self.end = self.now + timedelta(days=7)
+        start = timezone.now()
+        end = start + timedelta(days=7)
+
+        self.job = ScheduledJob.objects.create(
+            time_slot_start=start, time_slot_end=end,
+            callable_name="func", args=(), kwargs={})
 
     def test_default_status(self):
-        job = ScheduledJob.objects.create(
-                time_slot_start=self.start, time_slot_end=self.end)
-        self.assertEqual(job.status, ScheduledJob.STATUS_SCHEDULED)
+        self.assertEqual(
+            self.job.status,
+            ScheduledJob.STATUS_SCHEDULED)
 
     def test_repr(self):
-        job = ScheduledJob.objects.create(
-                time_slot_start=self.start,
-                time_slot_end=self.end,
-                callable_name="func")
-
         self.assertEqual(
             "<ScheduledJob (scheduled) callable='func'>",
-            repr(job)
+            repr(self.job)
         )
